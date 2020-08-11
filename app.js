f3dc195e1ac4e7d75155b5801d291a9386cd7e69
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-08-12 00:07:55 +0530

    Exports Module added

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+// console.log(date);
 
 const app = express();
 let items = ["Buy Food", "Cook Food", "Eat Food"];
@@ -10,11 +13,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-	let today = new Date();
-
-	let options = { weekday: "long", day: "numeric", month: "long" };
-	let day = today.toLocaleDateString("en-US", options);
-
+	let day = date.getDate();
 	res.render("list", { listTitle: day, newListItem: items });
 });
 

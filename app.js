800d8e45a030017c3f89b1d83f5371c13867d8c8
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-08-11 19:33:53 +0530

    EJS File Add

--- app.js
+++ app.js
@@ -3,8 +3,36 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-	res.send("Hello Raand");
+	var today = new Date();
+	var currentDay = today.getDay();
+	var day = "";
+	switch (currentDay) {
+		case 0:
+			day = "Raviwar";
+			break;
+		case 1:
+			day = "Somwar";
+			break;
+		case 2:
+			day = "Mangalwar";
+			break;
+		case 3:
+			day = "Budhwar";
+			break;
+		case 4:
+			day = "Guruwar";
+			break;
+		case 5:
+			day = "Shukrawar";
+			break;
+		case 6:
+			day = "Shaniwar";
+			break;
+	}
+	res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
